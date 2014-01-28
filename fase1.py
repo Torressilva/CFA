7da06f9bd957d58fbee0d8a2c553bc3974887af1
Juan Enrique Bonilla Morales <jbonilla.enriquem@gmail.com>
2014-01-28 17:53:52 -0600

    Limpieza de código
    Se borró el código innecesario

--- fase1.py
+++ fase1.py
@@ -8,19 +8,8 @@ __date__ ="$25-ene-2014 12:36:13$"
 import nltk
 
 fecha = "2@/Sept./199o";
-#fecha = fecha.split('/');
-#print fecha
 
-#tokens1 = nltk.regexp_tokenize(fecha, '''\w+''')
-#print tokens1
-
-tokens2 = nltk.regexp_tokenize(fecha, '''\W+''')
-print tokens2
-#print fecha
+tokens = nltk.regexp_tokenize(fecha, '''\W+''')
+print tokens
 
 print fecha.split('/')
-#letras = [''.join(c for c in s if c.isalnum()) for s in fecha.split()]
-#print letras
-
-#num = [''.join(c for c in s if c.isdigit()) for s in fecha.split()]
-#print num
