cbf915ca0e7b526f7077d6fd4f3493087525fa82
Ernesto Javier Ramírez Abitia <ernestorab@gmail.com>
2014-01-29 10:31:21 -0600

    Identificación del posible separador de la fecha.
    Se usa el diccionario inverso para identificar el carácter que posiblemente es un separador de la fecha. Se usa el carácter para separar la fecha.
    Se comentó el código.

--- fase1.py
+++ fase1.py
@@ -9,16 +9,32 @@ import nltk
 
 fecha = "2@/Sept./199o";
 
+#Se obtienen cualquier caracter que no sea alfanumerico
 tokens = nltk.regexp_tokenize(fecha, '''\W+''')
 
+#lo juntamos en una sola palabra para poder hacer el conteo de caracteres
 tokens = ''.join(tokens)
-
+print tokens
+#Cuenta las veces que aparece un caracter. Lo guarda en un diccionario.
 c = Counter(tokens)
-inv_map = {}
+print c
+#Se necesita ahora acceder a los valores del diccionario, que son el numero
+#de repeticiones de cada caracter (claves).
+inv_c = {}
 for k,v in c.iteritems():
-    inv_map[v]=inv_map.get(v,[])
-    inv_map[v].append(k)
+    inv_c[v]=inv_c.get(v,[])
+    inv_c[v].append(k)
 
-print inv_map.items()
+print inv_c.items()
 
-print fecha.split('/')
+#Obtener el caracter que se repite dos veces
+#Si no hay o si hay varios de ellos, entonces no se puede saber exactamente que
+#caracter es el separador y ese caso se deja para la siguiente fase.
+if inv_c.has_key(2) & len(inv_c[2])==1:
+    #Se accede primero al caracter que tenga 2 repeticiones, esta regresa una
+    #lista con un solo elemento. Accesamos el primer y unico elemento de la
+    #lista con el comando [0].
+    print fecha.split(inv_c[2][0])
+else:
+    #No se puede resolver en la fase 1. Marcar elemento para mandarlo a la fase2
+    print 'Mandar a la fase 2'
